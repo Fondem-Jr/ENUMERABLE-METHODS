e081767bf2edfef5938d56a3fa4b67d63956e4db
Fondem Junior <fondemjunior@yahoo.com>
2021-04-20 22:52:47 +0100

    Add linters

--- script.rb
+++ script.rb
@@ -1,12 +1,11 @@
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
 
-    def my_each (array) 
-        i = 0
-        while i < array.length
-          yield array[i]
-  
-          i += 1
-           end
-    end
-  
-    array = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
-    my_each(array) { |friend| puts "Hello, " + friend }
+    i += 1
+  end
+end
+
+array = %w[Sharon Leo Leila Brian Arun]
+my_each(array) { |friend| puts "Hello, #{friend}" }
