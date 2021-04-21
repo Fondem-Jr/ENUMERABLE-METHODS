e1ddbb22fa7a7cd36adc9108698080ae17c156fe
JuliCarracedo <julicarra@hotmail.com>
2021-04-21 12:57:04 -0300

    Include addition to Enumerables for use of dot operator

--- script.rb
+++ script.rb
@@ -1,69 +1,66 @@
-def my_each(array)
-  i = 0
-  while i < array.length
-    yield array[i]
+module Enumerable
+  def my_each
+    i = 0
+    while i < self.length
+      yield self[i]
+      i += 1
+    end
+  end
 
-    i += 1
+  def my_each_with_index
+    while i < self.length
+      yield self[i], i
+
+      i += 1
+    end
+  end
+
+  def my_select
+    i = 0
+    result = []
+    while i < self.length
+      filter = yield self[i]
+      result.push(self[i]) if filter
+      i += 1
+    end
+    result
+  end
+
+  def my_all?
+    i = 0
+    while i < self.length
+      result = yield self[i]
+      return result unless result
+
+      i += 1
+    end
+    true
+  end
+
+  def my_any?
+    i = 0
+    while i < self.length
+      result = yield self[i]
+      return result if result
+
+      i += 1
+    end
+    false
+  end
+
+
+  def my_none?
+    i = 0
+    while i < self.length
+      result = yield self[i]
+      return false if result
+
+      i += 1
+    end
+    true
   end
 end
 
-def my_each_with_index(array)
-  i = 0
-  while i < array.length
-    yield array[i], i
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    i += 1
-  end
-end
-
-def my_select(array)
-  i = 0
-  result = []
-  while i < array.length
-    filter = yield array[i]
-    result.push(array[i]) if filter
-    i += 1
-  end
-  result
-end
-
-def my_all?(array)
-  i = 0
-  while i < array.length
-    result = yield array[i]
-    return result unless result
-
-    i += 1
-  end
-  true
-end
-
-def my_any?(array)
-  i = 0
-  while i < array.length
-    result = yield array[i]
-    return result if result
-
-    i += 1
-  end
-  false
-end
-
-
-def my_none?(array)
-  i = 0
-  while i < array.length
-    result = yield array[i]
-    return false if result
-
-    i += 1
-  end
-  true
-end
-
-array = [1, 1, 2, 2]
-puts my_none?(array) { |num| (num % 3).zero? }
-
-array = [1, 2, 4, 7]
-puts my_any?(array) { |num| (num % 3).zero? }
-
+puts array.my_select {|num| num%3 == 0} 
